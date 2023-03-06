cfee4b6d4700dd431682fb5a06407f141eb059d6
Joe <gramstad.j@northeastern.edu>
2023-03-05 23:05:58 -0600

    added comprehension for tables

--- wiki_scrape.py
+++ wiki_scrape.py
@@ -2,6 +2,18 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+
+CONFIGURATION = {
+    "Actor": {
+
+    }
+
+
+
+}
+
+
+
 def scrape_wiki_article(url, num_columns, start_year):
     response = requests.get(
         url=url,
@@ -16,36 +28,37 @@ def scrape_wiki_article(url, num_columns, start_year):
     position = None
     insert_value = None
 
+    tables = [x for x in tables if 'plainrowheaders' not in x['class'] ]
+
     for table in tables:
-        if 'plainrowheaders' not in set(table['class']):
-            data_rows = table.find_all('tr')
+        data_rows = table.find_all('tr')
 
-            for row in data_rows:
-                winners = []
-                data_elems = row.find_all('td')
-                cur_index = 0
+        for row in data_rows:
+            winners = []
+            data_elems = row.find_all('td')
+            cur_index = 0
 
-                for elem in data_elems:
-                    if cur_index >= num_columns:
-                        year += 1
-                        winners.append("Winner")
-                        continue
-                    if "rowspan" in elem.attrs:
-                        remaining_rows = int(elem['rowspan'])
-                        position = cur_index
-                        insert_value = elem.text.strip()
+            for elem in data_elems:
+                if cur_index >= num_columns:
+                    year += 1
+                    winners.append("Winner")
+                    continue
+                if "rowspan" in elem.attrs:
+                    remaining_rows = int(elem['rowspan'])
+                    position = cur_index
+                    insert_value = elem.text.strip()
 
-                    else:
-                        winners.append(elem.text.strip())
+                else:
+                    winners.append(elem.text.strip())
 
-                    cur_index += 1
+                cur_index += 1
 
-                if remaining_rows > 0:
-                    winners.insert(position, insert_value)
-                    remaining_rows -= 1
+            if remaining_rows > 0:
+                winners.insert(position, insert_value)
+                remaining_rows -= 1
 
-                winners.insert(0, year)
-                print(winners)
+            winners.insert(0, year)
+            print(winners)
 
 
 def main(argv):
