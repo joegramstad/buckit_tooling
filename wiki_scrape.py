ac05afd7bd6612a252b3cf7af5c146f88b574af7
Joe <gramstad.j@northeastern.edu>
2023-03-11 09:42:30 -0600

    added csv insertion

--- wiki_scrape.py
+++ wiki_scrape.py
@@ -1,23 +1,15 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+import csv
 
+def scrape_wiki_article(url, num_columns, start_year, award):
 
-CONFIGURATION = {
-    "Actor": {
-
-    }
-
-
-
-}
-
-
-
-def scrape_wiki_article(url, num_columns, start_year):
     response = requests.get(
         url=url,
     )
+
+    master_list = []
 
     year = start_year - 1
 
@@ -28,7 +20,7 @@ def scrape_wiki_article(url, num_columns, start_year):
     position = None
     insert_value = None
 
-    tables = [x for x in tables if 'plainrowheaders' not in x['class'] ]
+    tables = [x for x in tables if 'plainrowheaders' not in x['class']]
 
     for table in tables:
         data_rows = table.find_all('tr')
@@ -58,19 +50,25 @@ def scrape_wiki_article(url, num_columns, start_year):
                 remaining_rows -= 1
 
             winners.insert(0, year)
-            print(winners)
+            if len(winners) > 1:
+                master_list.append(winners)
 
+    csv_name = award + '.csv'
+    with open(csv_name, 'w', encoding="utf-8", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(master_list)
 
 def main(argv):
-    url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_" + argv[1]
+    award = argv[1]
+    url = "https://en.wikipedia.org/wiki/" + award
     num_columns = argv[2]
     start_year = argv[3]
-    scrape_wiki_article(url, int(num_columns), int(start_year))
+    scrape_wiki_article(url, int(num_columns), int(start_year), award)
 
 # Pass in three args:
-# 1. award (i.e. Actor)
+# 1. award (i.e. Academy_Award_for_Best_Actor)
 # 2. number of columns in table (i.e. 3)
 # 3. year of first award (i.e. 1928)
-# full example: python wiki_scrape.py Actor 3 1928
+# full example: python wiki_scrape.py Academy_Award_for_Best_Actor 3 1928
 
 main(sys.argv)
