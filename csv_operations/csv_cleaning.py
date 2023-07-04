040c68c516f679cc142eee4fabb5d265d3c0a2bf
Joe <gramstad.j@northeastern.edu>
2023-07-04 11:00:47 -0400

    organized file types

--- csv_operations/csv_cleaning.py
+++ csv_operations/csv_cleaning.py
@@ -1,7 +1,7 @@
 import csv
 import utilities as util
 
-with open('test.csv', 'r', encoding= 'unicode_escape') as csv_file:
+with open('../test.csv', 'r', encoding='unicode_escape') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     master_list = []
     for row in csv_reader:
