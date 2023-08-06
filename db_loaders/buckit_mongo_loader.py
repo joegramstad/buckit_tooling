9fd462ef79e223f4988d2df5f61e9d5f08b12410
Joe <gramstad.j@northeastern.edu>
2023-08-06 16:17:08 -0400

    removed year in collection_identifiers query

--- db_loaders/buckit_mongo_loader.py
+++ db_loaders/buckit_mongo_loader.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from pymongo import MongoClient
 
 URI = "mongodb://localhost:27017/buckit"
@@ -18,13 +16,13 @@ master_hierarchy = {}
 root_children = []
 collection_categories = collection_lists.find().distinct("collectionListCategory")
 
-
-def insert_to_mongo(name, bottom_level, children):
+def insert_to_mongo(name, bottom_level, children, owner=None):
     insert_dict = {
         "name": name,
         "bottomLevel": bottom_level
     }
     if bottom_level:
+        insert_dict["owner"] = owner
         insert_dict["collectionListChildren"] = children
     else:
         insert_dict["children"] = children
@@ -54,19 +52,21 @@ for collection_category in collection_categories:
             owner_hierarchy = type_hierarchy[collection_owner]
             owner_children = []
             for collection_name in collection_names:
-                # owner_hierarchy[collection_name] = {}
                 owner_hierarchy[collection_name] = []
+                # collection_identifiers = collection_lists.find({"collectionListCategory": collection_category,
+                #                                                 "collectionListType": collection_type,
+                #                                                 "collectionListOwner": collection_owner,
+                #                                                 "name": collection_name}, {"_id": 1, "year": 1})
                 collection_identifiers = collection_lists.find({"collectionListCategory": collection_category,
                                                                 "collectionListType": collection_type,
                                                                 "collectionListOwner": collection_owner,
-                                                                "name": collection_name}, {"_id": 1, "year": 1})
+                                                                "name": collection_name}, {"_id": 1})
                 name_children = owner_hierarchy[collection_name]
                 for collection_identifier in collection_identifiers:
                     name_children.append(collection_identifier['_id'])
-                    # name_hierarchy[collection_identifier['year']] = collection_identifier['_id']
 
 
-                collection_name_id = insert_to_mongo(collection_name, True, name_children)
+                collection_name_id = insert_to_mongo(collection_name, True, name_children, collection_owner)
                 owner_children.append(collection_name_id)
 
             collection_owner_id = insert_to_mongo(collection_owner, False, owner_children)
